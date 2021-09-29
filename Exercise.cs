1635ec78a7f459304450c67b4edb3cdfef54ffb3
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-09-29 09:21:32 +0200

    Add files via upload

---
+++ Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_01._01
+{
+    public class Exercise
+    {
+        public bool IsLessThan10(string input)
+        {
+            int x = Int32.Parse(input);
+            if (x < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
